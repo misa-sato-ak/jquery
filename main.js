4f63e8c84e080b3d242122c7e302d9fca7046a17
misa-sato-ak <akmisa519@gmail.com>
2024-07-03 13:16:33 +0900

    変更

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(function(){
-    $('#slider').slick();
+    $('#slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+})
 });
